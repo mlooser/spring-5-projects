1394595abcf3c17c7bf33d5219597ecbd61aa12d
mlooser <mlooser@gmail.com>
2020-04-10 13:06:33 +0200

    [reactivestudents] init project

---
+++ reactivestudents/src/main/java/org/mlooser/learn/spring/reactivestudents/ReactiveStudentsApplication.java
@@ -0,0 +1,13 @@
+package org.mlooser.learn.spring.reactivestudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStudentsApplication.class, args);
+	}
+
+}
